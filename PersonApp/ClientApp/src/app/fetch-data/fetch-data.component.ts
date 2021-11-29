cbc079d7262ad6b6d993c379802be4f37706fa6d
dheerajofficial <dheeraj.fullstack@gmail.com>
2021-11-29 23:14:06 +0530

    show person list added

--- PersonApp/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ PersonApp/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -6,18 +6,16 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './fetch-data.component.html'
 })
 export class FetchDataComponent {
-  public forecasts: WeatherForecast[] = [];
+  public persons: Person[] = [];
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<WeatherForecast[]>(baseUrl + 'weatherforecast').subscribe(result => {
-      this.forecasts = result;
+    http.get<Person[]>(baseUrl + 'person').subscribe(result => {
+      this.persons = result;
     }, error => console.error(error));
   }
 }
 
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
+interface Person {
+  firstName: string;
+  lastName: string;
 }
