795b6a83f33c0f69b1714ede089a411f97f3d62c
dheerajofficial <dheeraj.fullstack@gmail.com>
2021-11-29 23:13:26 +0530

    add form to home - create person

--- PersonApp/ClientApp/src/app/home/home.component.ts
+++ PersonApp/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+export class Person {
+  "FirstName" : string;
+  "LastName" : string;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  baseUrl: string;
+  http: HttpClient;
+  model = new Person();
+
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) { 
+    this.baseUrl = baseUrl;
+    this.http = http;
+  }
+
+  onSubmit(form:any) {
+    console.log(form.value)
+    this.http.post<Person>(this.baseUrl + 'person', form.value).subscribe(result => {
+      
+    }, error => console.error(error));
+  }
 }
